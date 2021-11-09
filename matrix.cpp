e979af428959f1e950d6aa2db6fc30b1dc28cbf2
LeovanGit <vova11072003@gmail.com>
2021-11-09 15:45:23 +0200

    final

--- matrix.cpp
+++ matrix.cpp
@@ -1,27 +1,56 @@
+/*
+                  _        _      
+  _ __ ___   __ _| |_ _ __(_)_  __
+ | '_ ` _ \ / _` | __| '__| \ \/ /
+ | | | | | | (_| | |_| |  | |>  < 
+ |_| |_| |_|\__,_|\__|_|  |_/_/\_\
+                                  
+ by Leovan
+*/
+
 #include <iostream>
 #include <ncurses.h>
 #include <vector>
 
 const std::string symbols = "1QW2ER3TY4UI5IO6P[7AS8DF]9FG0HJK/LNCXZ";
 const int symbols_len = symbols.size();
+const int frequency = 50;
 int height = -1, width = -1;
 
 class Line
 {
-public:
+private:
     int x;
     int y = 0;
-    int speed = 75;
-    
+    int speed = 1; 
+    int len;
+
+public: 
+    void Set_random_x()
+    {
+        x = rand() % width; // [0; width]
+    }
+
+    void Set_random_len()
+    {
+        len = height/4 + (rand() % height/2); // [height/4; 3*height/4]
+    }
+
     void Move()
     {
-        y = ((y >= height + height/2) ? 0 : y+1);
+        if (y >= height + len)
+        {
+            y = 0;
+            Set_random_x();
+            Set_random_len();
+        }
+        else y++;
     }
 
     void Draw()
     {
         // remove old symbol, max length of line = height/2
-        if (y >= height/2) mvaddch(y - height/2, x, ' ');
+        if (y >= len) mvaddch(y - len, x, ' ');
         
         // recolor to green previous symbol
         if (y > 0 && y <= height)
@@ -42,27 +71,39 @@ public:
 
 int main()
 {
-//    std::vector <Line> lines(10);
-
     initscr();
     curs_set(0);
 
-    Line line;
-
     getmaxyx(stdscr, height, width);
-
-    line.x = rand() % width; // [0; width]
     
     start_color();
     init_pair(1, COLOR_GREEN, COLOR_BLACK);
+
+    attron(COLOR_PAIR(1));
+    mvprintw(height/2-3, width/2-16,"                  _        _      ");
+    mvprintw(height/2-2, width/2-16,"  _ __ ___   __ _| |_ _ __(_)_  __");
+    mvprintw(height/2-1, width/2-16," | '_ ` _ \\ / _` | __| '__| \\ \\/ /");
+    mvprintw(height/2,   width/2-16," | | | | | | (_| | |_| |  | |>  < ");
+    mvprintw(height/2+1, width/2-16," |_| |_| |_|\\__,_|\\__|_|  |_/_/\\_\\");
+    attroff(COLOR_PAIR(1));
+    refresh();
+    napms(500);
+
+    std::vector <Line> lines(frequency);
+    for (int i = 0, l = lines.size(); i < l; i++)
+    {
+        lines[i].Set_random_x();
+        lines[i].Set_random_len();
+    }
+
     while(true)
     {
-
-        line.Draw();
-        line.Move();
-
+        for (int i = 0, l = lines.size(); i < l; i++)
+        {
+            lines[i].Draw();
+            lines[i].Move();
+        }
     }
-    getch();
 
     endwin();
 
